48f7f5910ff7352bcbef500f48d7e66323dc2a45
Hyeongbin Im <hbim@si-analytics.ai>
2024-09-05 12:26:49 +0900

    style: format code

--- scripts/genPopup.js
+++ scripts/genPopup.js
@@ -94,73 +94,73 @@
     const tdProps = `style="display: flex; align-items: center; padding: 4px 8px; color: #616161; border-right: 1px solid #CCC; border-bottom: 1px solid #CCC; white-space: pre-wrap; word-break: break-word;"`
 
     const popupHtml = new DOMParser().parseFromString(`
-    <div id="${popupId}" style="position: absolute; top: 24px; right: 24px; z-index: 100000;">
-      <div
-        style="
-          width: 446px;
-          border-radius: 8px;
-          background-color: #FAFAFA;
-          color: #222222;
-          font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', sans-serif;
-          box-shadow: 0 12px 48px 16px rgba(0, 0, 0, 0.12), 0 9px 28px 0 rgba(0, 0, 0, 0.2), 0 6px 16px -8px rgba(0, 0, 0, 0.32);
-        "
-      >
-        <header
+      <div id="${popupId}" style="position: absolute; top: 24px; right: 24px; z-index: 100000;">
+        <div
           style="
-            height: 44px;
-            background-color: #E7F2FF;
-            position: relative;
-            display: flex;
-            align-items: center;
-            padding: 0 20px;
-            border-bottom: 1px solid #E0E0E0;
-            border-radius: 8px 8px 0 0;
+            width: 446px;
+            border-radius: 8px;
+            background-color: #FAFAFA;
+            color: #222222;
+            font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', sans-serif;
+            box-shadow: 0 12px 48px 16px rgba(0, 0, 0, 0.12), 0 9px 28px 0 rgba(0, 0, 0, 0.2), 0 6px 16px -8px rgba(0, 0, 0, 0.32);
           "
         >
-          <div style="font-size: 18px; font-weight: 800;">${title}</div>
-          <button
+          <header
             style="
-              position: absolute;
-              top: 4px;
-              right: 14px;
-              width: 36px;
-              height: 36px;
-              background-color: transparent;
-              border: none;
+              height: 44px;
+              background-color: #E7F2FF;
+              position: relative;
               display: flex;
               align-items: center;
-              justify-content: center;
+              padding: 0 20px;
+              border-bottom: 1px solid #E0E0E0;
+              border-radius: 8px 8px 0 0;
             "
-            onclick="(() => {
-              const oldPopup = document.getElementById('${popupId}')
-              if (oldPopup) {
-                document.body.removeChild(oldPopup)
-              }
-            })()"
           >
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-              <path d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z" fill="#616161"/>
-            </svg>
-          </button>
-        </header>
-        <main style="padding: 12px 20px 18px; display: flex; flex-flow: column nowrap; gap: 6px;">
-          <time style="display: flex; align-items: center; gap: 6px;">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-              <path d="M16.6641 2.50065H15.8307V0.833984H14.1641V2.50065H5.83073V0.833984H4.16406V2.50065H3.33073C2.41406 2.50065 1.66406 3.25065 1.66406 4.16732V17.5006C1.66406 18.4173 2.41406 19.1673 3.33073 19.1673H16.6641C17.5807 19.1673 18.3307 18.4173 18.3307 17.5006V4.16732C18.3307 3.25065 17.5807 2.50065 16.6641 2.50065ZM16.6641 17.5006H3.33073V8.33398H16.6641V17.5006ZM16.6641 6.66732H3.33073V4.16732H16.6641V6.66732Z" fill="#487AFF"/>
-            </svg>
-            <div style="font-size: 18px; font-weight: 700; line-height: 28px;">${dateText}</div>
-          </time>
-          <div style="display: grid; grid-template-rows: repeat(5, minmax(32px, auto)); grid-template-columns: 106px 1fr; border-top: 1px solid #CCC; border-left: 1px solid #CCC; font-size: 14px;">
-            <div ${thProps}>총 근무일</div><div ${tdProps}>${totalWorkingDaysCount}일</div>
-            <div ${thProps}>남은 근무일</div><div ${tdProps}>${remainingWorkingDaysCount}일</div>
-            <div ${thProps}>남은 재택근무</div><div ${tdProps}>${remainingWfhCount}일</div>
-            <div ${thProps}>내 현황</div><div ${tdProps}>${myStatus}</div>
-            <div ${thProps}>요약</div><div ${tdProps}>${summary}</div>
-          </div>
-        </main>
+            <div style="font-size: 18px; font-weight: 800;">${title}</div>
+            <button
+              style="
+                position: absolute;
+                top: 4px;
+                right: 14px;
+                width: 36px;
+                height: 36px;
+                background-color: transparent;
+                border: none;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+              "
+              onclick="(() => {
+                const oldPopup = document.getElementById('${popupId}')
+                if (oldPopup) {
+                  document.body.removeChild(oldPopup)
+                }
+              })()"
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                <path d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z" fill="#616161"/>
+              </svg>
+            </button>
+          </header>
+          <main style="padding: 12px 20px 18px; display: flex; flex-flow: column nowrap; gap: 6px;">
+            <time style="display: flex; align-items: center; gap: 6px;">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                <path d="M16.6641 2.50065H15.8307V0.833984H14.1641V2.50065H5.83073V0.833984H4.16406V2.50065H3.33073C2.41406 2.50065 1.66406 3.25065 1.66406 4.16732V17.5006C1.66406 18.4173 2.41406 19.1673 3.33073 19.1673H16.6641C17.5807 19.1673 18.3307 18.4173 18.3307 17.5006V4.16732C18.3307 3.25065 17.5807 2.50065 16.6641 2.50065ZM16.6641 17.5006H3.33073V8.33398H16.6641V17.5006ZM16.6641 6.66732H3.33073V4.16732H16.6641V6.66732Z" fill="#487AFF"/>
+              </svg>
+              <div style="font-size: 18px; font-weight: 700; line-height: 28px;">${dateText}</div>
+            </time>
+            <div style="display: grid; grid-template-rows: repeat(5, minmax(32px, auto)); grid-template-columns: 106px 1fr; border-top: 1px solid #CCC; border-left: 1px solid #CCC; font-size: 14px;">
+              <div ${thProps}>총 근무일</div><div ${tdProps}>${totalWorkingDaysCount}일</div>
+              <div ${thProps}>남은 근무일</div><div ${tdProps}>${remainingWorkingDaysCount}일</div>
+              <div ${thProps}>남은 재택근무</div><div ${tdProps}>${remainingWfhCount}일</div>
+              <div ${thProps}>내 현황</div><div ${tdProps}>${myStatus}</div>
+              <div ${thProps}>요약</div><div ${tdProps}>${summary}</div>
+            </div>
+          </main>
+        </div>
       </div>
-    </div>
-  `, 'text/html')
+    `, 'text/html')
 
     document.body.appendChild(popupHtml.body.firstChild)
   }
