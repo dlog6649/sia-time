9b6742661d4c56207454ccd65001d35ed1939286
hbim <hbim@si-analytics.ai>
2025-07-09 10:44:22 +0900

    style: change to singleQuote

--- scripts/genPopup.js
+++ scripts/genPopup.js
@@ -1,12 +1,13 @@
 (() => {
-  const title = "시아시간"
-  const popupId = "sia-time-popup-fj23CzEq"
+  const title = '시아시간'
+  const popupId = 'sia-time-popup-fj23CzEq'
   // 매달 제공되는 재택근무일수
   const wfhCountPerMonth = 2
 
   const convertSignificantToHours = (significant) => {
-    const dayRegexp = /(재택근무|연차|휴가|공가|병가|휴직|결혼|회갑|출산|사망|탈상|예비군|훈련소)/g
-    if (dayRegexp.test(significant) || significant === "기타") {
+    const dayRegexp =
+      /(재택근무|연차|휴가|공가|병가|휴직|결혼|회갑|출산|사망|탈상|예비군|훈련소)/g
+    if (dayRegexp.test(significant) || significant === '기타') {
       return 8
     }
 
@@ -24,36 +25,48 @@
       document.body.removeChild(oldPopup)
     }
 
-    const summaryTable = document.querySelector("#_content")?.contentDocument.querySelector("#grid3")
-    const lackTimeIdx = [...(summaryTable?.querySelector("thead")?.querySelectorAll("th") ?? [])]
-      .findIndex((th) => th.innerText === "미달시간")
-    const lackTimeTxt = summaryTable?.querySelector("tbody")?.querySelector("tr")?.childNodes[lackTimeIdx]?.innerText ?? ""
-    const [lackH, lackM] = lackTimeTxt.split(":").map(Number)
+    const summaryTable = document
+      .querySelector('#_content')
+      ?.contentDocument.querySelector('#grid3')
+    const lackTimeIdx = [
+      ...(summaryTable?.querySelector('thead')?.querySelectorAll('th') ?? []),
+    ].findIndex((th) => th.innerText === '미달시간')
+    const lackTimeTxt =
+      summaryTable?.querySelector('tbody')?.querySelector('tr')?.childNodes[lackTimeIdx]
+        ?.innerText ?? ''
+    const [lackH, lackM] = lackTimeTxt.split(':').map(Number)
     const lackMins = lackH * 60 + lackM
 
-    const detailTable = document.querySelector("#_content")?.contentDocument.querySelector("#grid2")
-    const ths = [...(detailTable?.querySelector("thead")?.querySelectorAll("th") ?? [])]
-    const dateIdx = ths.findIndex((th) => th.innerText === "일자")
-    const significantIdx = ths.findIndex((th) => th.innerText === "특이사항")
-    const bodyTrs = [...(detailTable?.querySelector("tbody")?.querySelectorAll("tr") ?? [])]
+    const detailTable = document
+      .querySelector('#_content')
+      ?.contentDocument.querySelector('#grid2')
+    const ths = [...(detailTable?.querySelector('thead')?.querySelectorAll('th') ?? [])]
+    const dateIdx = ths.findIndex((th) => th.innerText === '일자')
+    const significantIdx = ths.findIndex((th) => th.innerText === '특이사항')
+    const bodyTrs = [...(detailTable?.querySelector('tbody')?.querySelectorAll('tr') ?? [])]
     const workingDayTrs = bodyTrs.filter((tr) => !tr.childNodes[dateIdx]?.firstElementChild)
     const totalWorkingDaysCount = workingDayTrs.length
 
     const startOfToday = new Date()
     startOfToday.setHours(0, 0, 0, 0)
 
-    const remainingWorkingDayTrs = workingDayTrs.filter((tr) => new Date(tr.childNodes[dateIdx]?.innerText).getTime() >= startOfToday.getTime())
+    const remainingWorkingDayTrs = workingDayTrs.filter(
+      (tr) => new Date(tr.childNodes[dateIdx]?.innerText).getTime() >= startOfToday.getTime(),
+    )
 
-    const subtractedMinsInLackTime = remainingWorkingDayTrs.reduce((acc, tr) => {
-      const significants = tr.childNodes[significantIdx]?.innerText.split("\n")
-      const hours = significants.reduce((acc, sig) => (acc + convertSignificantToHours(sig)), 0)
+    const subtractedMinsInLackTime =
+      remainingWorkingDayTrs.reduce((acc, tr) => {
+        const significants = tr.childNodes[significantIdx]?.innerText.split('\n')
+        const hours = significants.reduce((acc, sig) => acc + convertSignificantToHours(sig), 0)
 
-      return acc + hours
-    }, 0) * 60
+        return acc + hours
+      }, 0) * 60
 
     const remainingWorkingDaysCount = remainingWorkingDayTrs.length
 
-    const usedWfhCount = workingDayTrs.filter((tr) => tr.childNodes[significantIdx]?.innerText === "재택근무").length
+    const usedWfhCount = workingDayTrs.filter(
+      (tr) => tr.childNodes[significantIdx]?.innerText === '재택근무',
+    ).length
     const remainingWfhCount = wfhCountPerMonth - usedWfhCount
 
     const pageDate = new Date(bodyTrs[0]?.childNodes[dateIdx]?.innerText)
@@ -62,8 +75,8 @@
     const totalRemainingMins = remainingWorkingDaysCount * 8 * 60
 
     const toTimeText = (h, m) => {
-      const hours = h > 0 ? `${h}시간` : ""
-      const mins = m > 0 ? `${m}분` : ""
+      const hours = h > 0 ? `${h}시간` : ''
+      const mins = m > 0 ? `${m}분` : ''
 
       return `${hours} ${mins}`.trim()
     }
@@ -74,7 +87,7 @@
     const savingM = uSavingMins % 60
     const myStatus =
       savingH + savingM === 0
-        ? "저축한 시간이 없어요."
+        ? '저축한 시간이 없어요.'
         : `${toTimeText(savingH, savingM)} ${
           savingMins < 0
             ? "<span style='color: #EF2B2A; font-weight: 700;'>대출</span>"
@@ -87,15 +100,16 @@
     const summaryM = Math.round(minsPerDay % 60)
     const summary =
       remainingWorkingDaysCount === 0
-        ? "이달의 근무가 끝났어요."
+        ? '이달의 근무가 끝났어요.'
         : remainingMins < 0
-          ? "미달시간이 없어요."
+          ? '미달시간이 없어요.'
           : `앞으로 하루에 ${toTimeText(summaryH, summaryM)}씩 근무하면 돼요.`
 
     const thProps = `style="background-color: #F5F5F5; display: flex; align-items: center; padding: 4px 8px; border-right: 1px solid #CCC; border-bottom: 1px solid #CCC;"`
     const tdProps = `style="display: flex; align-items: center; padding: 4px 8px; color: #616161; border-right: 1px solid #CCC; border-bottom: 1px solid #CCC; white-space: pre-wrap; word-break: break-word;"`
 
-    const popupHtml = new DOMParser().parseFromString(`
+    const popupHtml = new DOMParser().parseFromString(
+      `
       <div id="${popupId}" style="position: absolute; top: 24px; right: 24px; z-index: 100000;">
         <div
           style="
@@ -164,7 +178,9 @@
           </main>
         </div>
       </div>
-    `, 'text/html')
+    `,
+      'text/html',
+    )
 
     document.body.appendChild(popupHtml.body.firstChild)
   }
@@ -179,14 +195,14 @@
         return
       }
 
-      const clickedMenu = document.body.querySelector(".jstree-clicked")?.lastChild?.data
+      const clickedMenu = document.body.querySelector('.jstree-clicked')?.lastChild?.data
 
       if (!clickedMenu) {
         triedCount++
         return
       }
 
-      if (clickedMenu !== "근무시간집계현황") {
+      if (clickedMenu !== '근무시간집계현황') {
         const oldPopup = document.getElementById(popupId)
         if (oldPopup) {
           document.body.removeChild(oldPopup)
@@ -195,22 +211,28 @@
         return
       }
 
-      const loadings = [...(document.querySelector("#_content")?.contentDocument.querySelectorAll(".PUDD-UI-loading") ?? [])]
+      const loadings = [
+        ...(document
+          .querySelector('#_content')
+          ?.contentDocument.querySelectorAll('.PUDD-UI-loading') ?? []),
+      ]
 
-      if (loadings.length !== 4 || !loadings.every((l) => l.style.display === "none")) {
+      if (loadings.length !== 4 || !loadings.every((l) => l.style.display === 'none')) {
         triedCount++
         return
       }
 
-      const searchBtn = document.querySelector("#_content")?.contentDocument.querySelector("#searchBtn")
+      const searchBtn = document
+        .querySelector('#_content')
+        ?.contentDocument.querySelector('#searchBtn')
 
       if (!searchBtn) {
         triedCount++
         return
       }
 
-      searchBtn.removeEventListener("click", genPopupWhenReady)
-      searchBtn.addEventListener("click", genPopupWhenReady)
+      searchBtn.removeEventListener('click', genPopupWhenReady)
+      searchBtn.addEventListener('click', genPopupWhenReady)
 
       genContent()
       clearInterval(intervalId)
